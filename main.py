77f75a438ccff3a110ee861becdfa43aac7136f4
mucahit <mucahitkutsal1717@gmail.com>
2022-03-30 20:39:51 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Optional
+import pymongo
+from fastapi import FastAPI
+
+
+uri = "mongodb://saasmongodb:9UlcfXu1mJIVUsucZ4aKkp043hUYG0C8ZXFlBfpPx0nIVrT3sFgEY0uX56cbIJfM6ycvCMjtfqs05FgY5LWSRg==@saasmongodb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@saasmongodb@"
+myclient = pymongo.MongoClient(uri)
+mydb = myclient["saas"]
+mycol = mydb["filter"]
+
+app = FastAPI()
+
+
+@app.get("/filter")
+def read_root():
+    return {'data':[x['domain'] for x in mycol.find({},{ "_id": 0 })]}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
